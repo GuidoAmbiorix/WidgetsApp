f893d8a2b8fd6905052ef959d88884d55e5cbbad
GuidoAmbiorix <guidoambiorix@gmail.com>
2023-07-09 09:13:33 -0400

    --Se inicio el menuItems

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.purple,
+  Colors.amber
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'Selected colors must be greater than 0'),
+        assert(selectedColor < colorList.length,
+            'Selected colors must be less or equal than ${colorList.length}');
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(centerTitle: false),
+      );
+}
